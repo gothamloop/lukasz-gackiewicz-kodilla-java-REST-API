b7b63c4084634c6a254df684d4393db7b5c14d5a
gothamloop <g.otham@webtunnelo.com>
2020-09-08 21:29:36 +0200

    JAR and PostgreSQL fixed

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication extends SpringBootServletInitializer {
+public class TasksApplication { //extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 	//	TaskDto taskDto = new TaskDto(
@@ -23,8 +23,8 @@ public class TasksApplication extends SpringBootServletInitializer {
 		SpringApplication.run(TasksApplication.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(TasksApplication.class);
-	}
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(TasksApplication.class);
+//	}
 }
