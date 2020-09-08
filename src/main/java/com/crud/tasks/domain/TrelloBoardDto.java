25e87a3a0598c5613ea50121b06c397ebcaaa432
gothamloop <g.otham@webtunnelo.com>
2020-09-08 21:08:28 +0200

    First Commit

--- src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -1,2 +1,14 @@
-package com.crud.tasks.domain;public class TrelloBoardDto {
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TrelloBoardDto {
+    private String name;
+    private String id;
 }
