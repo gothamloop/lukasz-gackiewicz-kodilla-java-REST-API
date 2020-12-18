2c7d2ac7f8112dea5c35f200c0908e9c68654cc5
gothamloop <g.otham@webtunnelo.com>
2020-12-18 17:41:41 +0100

    First Commit

---
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -0,0 +1,35 @@
+package com.crud.tasks.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class StaticWebPageController {
+
+//    @RequestMapping("/")
+//    public String index(Map<String, String> model) {
+//        model.put("variable", "My Thymeleaf variable");
+//        return "index";
+//    }
+
+//    @RequestMapping("/")
+//    public String index(Map<String, Object> model) {
+//        model.put("variable", "My Thymeleaf variable");
+//        model.put("one", 1);
+//        model.put("two", 2);
+//        return "index";
+//    }
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
+        model.put("sample_one", "2 * 2 = ");
+        model.put("sample_two", "2 * 2 + 2 = ");
+        model.put("sample_three", "2 - 2 * 2 = ");
+        return "index";
+    }
+}
