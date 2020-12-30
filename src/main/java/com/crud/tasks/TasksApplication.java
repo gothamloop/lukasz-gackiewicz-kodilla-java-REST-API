bbac13046de6f59390c20a2320526aaee7865a1f
gothamloop <g.otham@webtunnelo.com>
2020-12-30 19:20:19 +0100

    First Commit

--- src/main/java/com/crud/tasks/TasksApplication.java
+++ src/main/java/com/crud/tasks/TasksApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TasksApplication {
-	//public class TasksApplication extends SpringBootServletInitializer {
+//public class TasksApplication {
+	public class TasksApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 	//	TaskDto taskDto = new TaskDto(
@@ -25,8 +25,8 @@ public class TasksApplication {
 	}
 
 	// Dla Heroku zaremowac
-	//@Override
-	//protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	//	return application.sources(TasksApplication.class);
-	//}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TasksApplication.class);
+	}
 }
